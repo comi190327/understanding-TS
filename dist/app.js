ccab67ddc98ad92605dc564c36655b03953f39de
comi190327 <comi@actails.com>
2024-05-02 18:23:46 +0900

    Getter & Setterまで学習

--- dist/app.js
+++ dist/app.js
@@ -18,29 +18,53 @@ class Department {
 }
 class ITDepartment extends Department {
     constructor(id, admins) {
-        super(id, 'IT');
+        super(id, "IT");
         this.admins = admins;
     }
 }
 class AccountingDepartment extends Department {
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("レポートが見つかりません。");
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("正しい値を設定してください。");
+        }
+        this.addReport(value);
+    }
     constructor(id, reports) {
-        super(id, 'Accounting');
+        super(id, "Accounting");
         this.reports = reports;
+        this.lastReport = reports[0];
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReports() {
         console.log(this.reports);
     }
+    addEmployee(name) {
+        if (name === "Max") {
+            this.employees.push(name);
+        }
+    }
 }
-const it = new ITDepartment('d1', ['Max']);
-it.addEmployee('Max');
-it.addEmployee('Manu');
+const it = new ITDepartment("d1", ["Max"]);
+it.addEmployee("Max");
+it.addEmployee("Manu");
 it.describe();
 it.printEmployeeInformation();
 console.log(it);
-const accouting = new AccountingDepartment('d2', []);
-accouting.addReport('Someting');
+const accouting = new AccountingDepartment("d2", []);
+accouting.mostRecentReport = "通期会計レポート";
+accouting.addReport("Someting");
 accouting.printReports();
+console.log(accouting.mostRecentReport);
+accouting.addEmployee("Max");
+accouting.addEmployee("Manu");
+accouting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,77 @@
 class Department {
-    // private id: string;
-    // name: string;
-    private employees: string[] = [];
+  // private id: string;
+  // name: string;
+  protected employees: string[] = [];
 
-    constructor(private readonly id: string, public name: string) {
-        // this.id = id;
-        // this.name = n;
-    }
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
 
-    describe(this: Department) {
-        console.log(`Department (${this.id}): ${this.name}`);
-    }
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
 
-    addEmployee(employee: string) {
-        this.employees.push(employee);
-    }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
 
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-
 class ITDepartment extends Department {
-    admins: string[];
-    constructor(id: string, admins: string[]) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
 }
 
 class AccountingDepartment extends Department {
-    constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting');
-    }
+  private lastReport: string;
 
-    addReport(text: string) {
-        this.reports.push(text);
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
     }
+    throw new Error("レポートが見つかりません。");
+  }
 
-    printReports() {
-        console.log(this.reports);
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("正しい値を設定してください。");
     }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      this.employees.push(name);
+    }
+  }
 }
 
-const it = new ITDepartment('d1', ['Max']);
+const it = new ITDepartment("d1", ["Max"]);
 
-it.addEmployee('Max');
-it.addEmployee('Manu');
-
+it.addEmployee("Max");
+it.addEmployee("Manu");
 
 // console.log(accouting);
 
@@ -57,6 +79,15 @@ it.describe();
 it.printEmployeeInformation();
 console.log(it);
 
-const accouting = new AccountingDepartment('d2', []);
-accouting.addReport('Someting');
+const accouting = new AccountingDepartment("d2", []);
+
+accouting.mostRecentReport = "通期会計レポート";
+
+accouting.addReport("Someting");
 accouting.printReports();
+console.log(accouting.mostRecentReport);
+
+accouting.addEmployee("Max");
+accouting.addEmployee("Manu");
+
+accouting.printEmployeeInformation();
