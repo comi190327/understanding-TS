3284a18681d25463cc11d0683fe412a7ef4959df
comi190327 <comi@actails.com>
2024-05-03 23:21:28 +0900

    クラスでのインターフェースの実装を完了

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,15 @@
 "use strict";
-let user1;
-user1 = {
-    name: "Max",
-    age: 30,
+class Person {
+    constructor(n) {
+        this.age = 30;
+        this.name = n;
+    }
     greet(phrase) {
         console.log(phrase + " " + this.name);
-    },
-};
+    }
+}
+let user1;
+user1 = new Person("Max");
 user1.greet("Hello, I am");
+console.log(user1);
 //# sourceMappingURL=app.js.map
