3346e82ae615a9713cc1c549314e4e62a1d0548e
comi190327 <comi@actails.com>
2024-05-03 23:11:03 +0900

    最初のインターフェースを完了

---
+++ src/classes.ts
@@ -0,0 +1,121 @@
+abstract class Department {
+  static fiscalYear = 2024;
+  // private id: string;
+  // name: string;
+  protected employees: string[] = [];
+
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
+  constructor(protected readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  abstract describe(this: Department): void;
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+
+  describe() {
+    console.log("IT部門 - ID: " + this.id);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("レポートが見つかりません。");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("正しい値を設定してください。");
+    }
+    this.addReport(value);
+  }
+
+  private constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment("d2", []);
+    return this.instance;
+  }
+
+  describe() {
+    console.log("会計部門 - ID: " + this.id);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      this.employees.push(name);
+    }
+  }
+}
+
+const employee1 = Department.createEmployee("Max");
+console.log(employee1, Department.fiscalYear);
+
+const it = new ITDepartment("d1", ["Max"]);
+
+it.addEmployee("Max");
+it.addEmployee("Manu");
+
+// console.log(accouting);
+
+it.describe();
+it.printEmployeeInformation();
+console.log(it);
+
+// const accouting = new AccountingDepartment("d2", []);
+const accouting = AccountingDepartment.getInstance();
+const accouting2 = AccountingDepartment.getInstance();
+
+// console.log(accouting, accouting2);
+
+accouting.mostRecentReport = "通期会計レポート";
+
+accouting.addReport("Someting");
+// accouting.printReports();
+console.log(accouting.mostRecentReport);
+
+accouting.addEmployee("Max");
+accouting.addEmployee("Manu");
+
+// accouting.printEmployeeInformation();
+accouting.describe();
