03a066be73319ffe66373524198d7cbb67efaca6
comi190327 <comi@actails.com>
2024-05-05 14:42:11 +0900

    オプショナルチェインまで完了

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 const e1 = {
     name: "Max",
     privilages: ["create - server"],
@@ -12,4 +13,13 @@ function add2(a, b) {
 }
 const result = add2("Hello", " TypeScript");
 result.split(" ");
+const fetchedUserData = {
+    id: "u1",
+    name: "user1",
+    job: {
+        title: "Developer",
+        description: "TypeScript",
+    },
+};
+console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
 //# sourceMappingURL=app.js.map
