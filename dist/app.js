2c03c93c24b9ca472af7e37a7592ef9c3e6d2033
comi190327 <comi@actails.com>
2024-05-05 15:26:47 +0900

    組み込みのGenerics型＆Genericsとはまで完了

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,10 @@
 "use strict";
-var _a;
-const e1 = {
-    name: "Max",
-    privilages: ["create - server"],
-    startDate: new Date(),
-};
-function add2(a, b) {
-    if (typeof a === "string" || typeof b === "string") {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-const result = add2("Hello", " TypeScript");
-result.split(" ");
-const fetchedUserData = {
-    id: "u1",
-    name: "user1",
-    job: {
-        title: "Developer",
-        description: "TypeScript",
-    },
-};
-console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-const userInput = undefined;
-const storeData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
-console.log(storeData);
+const names = [];
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10);
+    }, 2000);
+});
+promise.then((data) => {
+});
 //# sourceMappingURL=app.js.map
