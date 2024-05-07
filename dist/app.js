4e574b620c945b0062b6eea0f4eab1ff4e841e4f
comi190327 <comi@actails.com>
2024-05-07 21:39:49 +0900

    プロパティデコレータの詳細

--- dist/app.js
+++ dist/app.js
@@ -36,4 +36,28 @@ Person = __decorate([
 ], Person);
 const pers = new Person();
 console.log(pers);
+function Log(target, propertyName) {
+    console.log("Property デコレータ");
+    console.log(target, propertyName);
+}
+class Product {
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("不正な価格です - 0以下は設定できません。");
+        }
+    }
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
 //# sourceMappingURL=app.js.map
