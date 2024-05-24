3ef064f4eac19aaf9f3a2d924c6ee521569173a0
comi190327 <comi@actails.com>
2024-05-24 22:39:56 +0900

    プロジェクト一覧の表示まで完了

--- src/app.ts
+++ src/app.ts
@@ -55,6 +55,44 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement; // template要素取得用
+  hostElement: HTMLDivElement; // template要素出力用
+  element: HTMLElement; // template内要素取得用
+
+  constructor(private type: "active" | "finished") {
+    // tamplateタグの内容を取得
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    // template内のタグの中身を再帰的に取得
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type === "active" ? "実行中プロジェクト" : "完了プロジェクト";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+//
+
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement; // template要素取得用
@@ -157,3 +195,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
